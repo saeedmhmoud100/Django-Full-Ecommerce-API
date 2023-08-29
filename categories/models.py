6442b44dcbd51865276a531261b229d849f5be44
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-08-29 18:39:26 +0300

    create category model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
+
+
+class Category(models.Model):
+    _id = models.IntegerField(blank=True,null=True)
+    name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=40,blank=True)
+    image = models.ImageField(upload_to='./uploads/categories')
+    createdAt = models.DateTimeField(auto_now_add=True, editable=False)
+    updatedAt = models.DateTimeField(auto_now=True, editable=False)
+
+    def save(self, *args, **kwargs):
+        if not self._id:
+            self._id = self.id
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args,**kwargs)
+
+    def __str__(self):
+        return self.name
