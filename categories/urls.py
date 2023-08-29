1b7ba8ebb04a6e62997815379c52eb2647459e2b
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-08-29 16:10:58 +0300

    starting in categories app

--- categories/urls.py
+++ categories/urls.py
@@ -18,7 +18,4 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include('categories.urls')),
 ]
