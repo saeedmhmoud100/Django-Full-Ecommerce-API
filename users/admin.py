c59a8c79cfded46159d3affa5cb79ee18959b387
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-09-03 12:53:45 +0300

    create user address model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from users.models import UserAddress
+
+
 # Register your models here.
+
+@admin.register(UserAddress)
+class UserAddressAdmin(admin.ModelAdmin):
+    exclude = ('_id', )
--- users/models.py
+++ users/models.py
@@ -1,4 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+
+class UserAddress(models.Model):
+    _id = models.IntegerField(blank=True, null=True)
+    alias = models.CharField(max_length=100)
+    details = models.TextField(max_length=500, null=True, blank=True)
+    phone = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    postalCode = models.CharField(max_length=10, null=True, blank=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='addresses')
+
+    def save(self, *args, **kwargs):
+        if not self._id:
+            self._id = self.id
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return str('(' + self.user.username + ') ' + self.alias)
