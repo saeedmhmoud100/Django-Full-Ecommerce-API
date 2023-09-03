803063d2b8493a434c6825a258ca486811532b6b
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-09-03 17:15:24 +0300

    create a login by username or email

--- project/settings.py
+++ project/settings.py
@@ -117,6 +117,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'users.backends.EmailLoginBackend',
+]
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
---
+++ users/backends.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.core.exceptions import MultipleObjectsReturned
+from django.db.models import Q
+
+User = get_user_model()
+
+
+class EmailLoginBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(
+                Q(username__iexact=username) |
+                Q(email__iexact=username)
+            )
+        except User.DoesNotExist:
+            return None
+        except MultipleObjectsReturned:
+            return User.objects.filter(email=username).order_by('id').first()
+
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
