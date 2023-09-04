c86602bbef32e621e62d71a45a72d5b6258cef9b
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-09-04 20:58:41 +0300

    solve signals problem in between user and cart models

--- users/signals.py
+++ users/signals.py
@@ -1,17 +1,14 @@
-# from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.contrib.auth import get_user_model
 from carts.models import Cart
-from users.models import MyUser
 
 
-@receiver(post_save, sender=MyUser)
+@receiver(post_save, sender=get_user_model())
 def create_user_cart(sender, instance, created, **kwargs):
-    print("Signal handler called!")
     if created:
         Cart.objects.create(user=instance)
 
 
 # Connect the signal
-post_save.connect(create_user_cart, sender=MyUser)
+post_save.connect(create_user_cart, sender=get_user_model())
