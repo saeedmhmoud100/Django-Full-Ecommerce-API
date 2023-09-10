0c41e3dad0747beca3ab7fcc0a8aee5e24e4eb48
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-09-10 21:41:15 +0300

    ordering the product data

--- products/serializers.py
+++ products/serializers.py
@@ -17,9 +17,13 @@ class ColorSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     colors = serializers.SerializerMethodField()
+    images = serializers.ImageField()
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ['id','title','slug','description','quantity','sold','price','imageCover',
+                  'images','colors',
+                  'user','category','brand', 'createdAt','updatedAt','_id']
+        # fields = '__all__'
 
     def get_colors(self,obj):
         return [color.name for color in obj.colors.all()]
