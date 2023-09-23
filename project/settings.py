a7d60cc930809377e5287c059774ae87a809f5d2
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-09-23 23:20:30 +0300

    solve data access problem

--- project/settings.py
+++ project/settings.py
@@ -237,9 +237,20 @@ DJOSER = {
 
 # ALLOWED_HOSTS=["*"]
 CORS_ALLOWED_ORIGINS = [
-    'https://*',
     'http://*',
+    'https://*',
     'http://localhost:3000',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ORIGIN_ALLOW_ALL = True
