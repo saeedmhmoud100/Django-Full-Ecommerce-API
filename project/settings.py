d64257aca77256c1d80c0b6711176b8a1b1d538f
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2023-09-24 12:58:11 +0300

    trying to solve Access-Control-Allow-Origin problem

--- project/settings.py
+++ project/settings.py
@@ -236,12 +236,15 @@ DJOSER = {
 }
 
 # ALLOWED_HOSTS=["*"]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://django-ecommerce-api.onrender.com',
     'http://*',
     'https://*',
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'https://django-ecommerce-api.onrender.com',
 ]
 
 CORS_ALLOW_HEADERS = [
@@ -255,10 +258,9 @@ CORS_ALLOW_HEADERS = [
     'x-csrftoken',
     'x-requested-with',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:8000',
-    'http://localhost:8000',
-    'https://django-ecommerce-api.onrender.com',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://127.0.0.1:8000',
+#     'http://localhost:8000',
+#     'https://django-ecommerce-api.onrender.com',
+# ]
